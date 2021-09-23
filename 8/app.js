b4ac0d828182b13b1499be6f6edbeb1efc53d131
DM-MRTNVCH <dmvshn@gmail.com>
2021-09-23 16:57:04 +0300

    8

--- 8/app.js
+++ 8/app.js
@@ -1,20 +1,57 @@
-
-const http = require('http')
-const PORT = 3005
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const PORT = 3005;
 
 const server = http.createServer((req, res) => {
-    console.log('server request');
-    console.log(req.url, req.method);
 
-    res.setHeader('Content-Type', 'application/json')
+    res.setHeader('Content-Type', 'text/html');
+    const createPath = page => path.resolve(__dirname, 'views', `${page}.html`);
 
-    const data = JSON.stringify([
-        {name: 'raz', age: 1},
-        {name: 'dva', age: 2}
-    ])
-    res.end(data)
-})
+    let basePath = '';
+    switch (req.url) {
+        case '/':
+        case '/home':
+        case '/index':
+            basePath = createPath('index');
+            res.statusCode = 200
+            break;
+        case '/about-us':
+            res.statusCode = 301
+            res.setHeader('Location', '/contacts')
+            res.end()
+        case '/contacts':
+            basePath = createPath('contacts');
+            res.statusCode = 200
+            break;
+        default:
+            basePath = createPath('error')
+            res.statusCode = 404
+    }
+    fs.readFile(basePath, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.statusCode = 500
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }
+    });
+
+    // res.write('<head><link rel="stylesheet" href="#"></head>')
+    // res.write('<h1>hello world!</h1>')
+    // res.write('<p>my name is dima</p>')
+    // res.end()
+
+    // const data = JSON.stringify([
+    //     {name: 'raz', age: 1},
+    //     {name: 'dva', age: 2}
+    // ])
+    //
+    // res.end(data)
+});
 
 server.listen(PORT, 'localhost', (error) => {
-    error ? console.log(error) : console.log(`listening port ${PORT}`)
-})
+    error ? console.log(error) : console.log(`listening port ${PORT}`);
+});
